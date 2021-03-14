5209c9bb89af065e1a7b387f64d5b1db74d1faa6
rickaa <37962604+d0l-phin@users.noreply.github.com>
2021-03-14 21:59:57 +0100

    📝 update

--- setup.py
+++ setup.py
@@ -2,12 +2,30 @@ from setuptools import setup, Extension  # type: ignore
 
 # read the contents of README
 from os import path
+import os
+
+
+library_dirs = []
+include_dirs = []
+
+
+if os.getenv("CUSTOM_INCLUDE", None) is not None:
+    include_dirs += [os.getenv("CUSTOM_INCLUDE")]
+
+if os.getenv("CUSTOM_LIBRARIES", None) is not None:
+    library_dirs += [os.getenv("CUSTOM_LIBRARIES")]
+
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-sqlite_uuid_ext = Extension("sqlite_uuid_ext", sources=["uuid.c"])
+sqlite_uuid_ext = Extension(
+    "sqlite_uuid_ext",
+    sources=["uuid.c"],
+    library_dirs=library_dirs,
+    include_dirs=include_dirs,
+)
 
 setup(
     name="sqlite_uuid",
